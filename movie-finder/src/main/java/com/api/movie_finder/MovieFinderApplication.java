b020e49bcdeee02d8c273b0bdb32ae1f6fc4a389
rshal <heiphin7and2@gmail.com>
2025-04-18 09:42:27 +0500

    Created Java Service

---
+++ movie-finder/src/main/java/com/api/movie_finder/MovieFinderApplication.java
@@ -0,0 +1,13 @@
+package com.api.movie_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinderApplication.class, args);
+	}
+
+}
---
+++ movie-finder/src/test/java/com/api/movie_finder/MovieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.movie_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
